89f86c4c440577d6591711efd2e3493e0a6cf678
mambax7 <mambax7@gmail.com>
2018-04-04 08:14:38 -0400

    updates
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- include/onupdate.php
+++ include/onupdate.php
@@ -17,6 +17,8 @@
  * @author       XOOPS Development Team
  */
 
+use XoopsModules\Xoopsmembers;
+
 if ((!defined('XOOPS_ROOT_PATH')) || !($GLOBALS['xoopsUser'] instanceof \XoopsUser)
     || !$GLOBALS['xoopsUser']->IsAdmin()
 ) {
@@ -48,7 +50,7 @@ function xoops_module_pre_update_xoopsmembers(\XoopsModule $module)
     /** @var Xoopsmembers\Helper $helper */
     /** @var Xoopsmembers\Utility $utility */
     $helper       = Xoopsmembers\Helper::getInstance();
-    $utility      = new \Xoopsmembers\Utility();
+    $utility      = new Xoopsmembers\Utility();
 
     $xoopsSuccess = $utility::checkVerXoops($module);
     $phpSuccess   = $utility::checkVerPhp($module);
@@ -71,25 +73,13 @@ function xoops_module_update_xoopsmembers(\XoopsModule $module, $previousVersion
 
     /** @var Xoopsmembers\Helper $helper */
     /** @var Xoopsmembers\Utility $utility */
-    /** @var Xoopsmembers\Configurator $configurator */
+    /** @var Xoopsmembers\Common\Configurator $configurator */
     $helper  = Xoopsmembers\Helper::getInstance();
-    $utility = new \Xoopsmembers\Utility();
-    $configurator = new \Xoopsmembers\Configurator();
+    $utility = new Xoopsmembers\Utility();
+    $configurator = new Xoopsmembers\Common\Configurator();
 
     if ($previousVersion < 240) {
 
-        //rename column EXAMPLE
-        $tables     = new Tables();
-        $table      = 'xoopsmembersx_categories';
-        $column     = 'ordre';
-        $newName    = 'order';
-        $attributes = "INT(5) NOT NULL DEFAULT '0'";
-        if ($tables->useTable($table)) {
-            $tables->alterColumn($table, $column, $attributes, $newName);
-            if (!$tables->executeQueue()) {
-                echo '<br>' . _AM_XXXXX_UPGRADEFAILED0 . ' ' . $migrate->getLastError();
-            }
-        }
 
         //delete old HTML templates
         if (count($configurator->templateFolders) > 0) {
@@ -136,7 +126,7 @@ function xoops_module_update_xoopsmembers(\XoopsModule $module, $previousVersion
         if (count($configurator->uploadFolders) > 0) {
             //    foreach (array_keys($GLOBALS['uploadFolders']) as $i) {
             foreach (array_keys($configurator->uploadFolders) as $i) {
-                $utilityClass::createFolder($configurator->uploadFolders[$i]);
+                $utility::createFolder($configurator->uploadFolders[$i]);
             }
         }
 
@@ -145,7 +135,7 @@ function xoops_module_update_xoopsmembers(\XoopsModule $module, $previousVersion
             $file = __DIR__ . '/../assets/images/blank.png';
             foreach (array_keys($configurator->copyBlankFiles) as $i) {
                 $dest = $configurator->copyBlankFiles[$i] . '/blank.png';
-                $utilityClass::copyFile($file, $dest);
+                $utility::copyFile($file, $dest);
             }
         }
 
@@ -153,9 +143,9 @@ function xoops_module_update_xoopsmembers(\XoopsModule $module, $previousVersion
         $sql = 'DELETE FROM ' . $GLOBALS['xoopsDB']->prefix('tplfile') . " WHERE `tpl_module` = '" . $module->getVar('dirname', 'n') . '\' AND `tpl_file` LIKE \'%.html%\'';
         $GLOBALS['xoopsDB']->queryF($sql);
 
-        /** @var XoopsGroupPermHandler $gpermHandler */
-        $gpermHandler = xoops_getHandler('groupperm');
-        return $gpermHandler->deleteByModule($module->getVar('mid'), 'item_read');
+        /** @var XoopsGroupPermHandler $grouppermHandler */
+        $grouppermHandler = xoops_getHandler('groupperm');
+        return $grouppermHandler->deleteByModule($module->getVar('mid'), 'item_read');
     }
     return true;
 }
