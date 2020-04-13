fb83d9a1a630426f3409325ddf7ab0d933bcd1d7
Lio <liomj83@gmail.com>
2020-04-13 10:39:41 +0800

    temporary fix for sort,limit,order

--- index.php
+++ index.php
@@ -34,14 +34,21 @@ $criteria = new \CriteriaCompo();
 
 $criteria->add(new \Criteria('level', 0, '>'));
 $validsort = ['uname', 'name', 'last_login', 'user_regdate', 'posts'];
-$sort      = (!in_array($helper->getModule()->getInfo('sortmembers'), $validsort)) ? 'uname' : $helper->getModule()->getInfo('sortmembers');
-
+//$sort      = (!in_array($helper->getModule()->getInfo('sortmembers'), $validsort)) ? 'uname' : $helper->getModule()->getInfo('sortmembers');
+//temp solution
+$sort = (!in_array($xoopsModuleConfig['sortmembers'], $validsort ) ) ? 'uname' : $xoopsModuleConfig['sortmembers'];
+    
 $order = 'ASC';
-$temp  = $helper->getModule()->getInfo('membersorder');
-if (isset($temp) && 'DESC' == $temp) {
+//$temp  = $helper->getModule()->getInfo('membersorder');
+//temp solution
+if ( isset( $xoopsModuleConfig['membersorder'] ) && $xoopsModuleConfig['membersorder'] == 'DESC' ) {
+//if (isset($temp) && 'DESC' == $temp) {
     $order = 'DESC';
 }
-$limit = \Xmf\Request::getInt('limit', 20, 'POST');
+
+//temp solution
+$limit = (!empty($xoopsModuleConfig['membersperpage'])) ? intval($xoopsModuleConfig['membersperpage']) : 20;
+//$limit = \Xmf\Request::getInt('limit', 20, 'POST');
 if (0 == $limit || $limit > 50) {
     $limit = 50;
 }
